7ddc0695a607711bd9fac2876a3cd07c19d7c254
Clinton Medbery <cmedber@g.cofc.edu>
2014-09-19 19:50:45 -0300

    Dessert class working now

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,35 @@
 class Dessert
-  def initialize(name, calories)
+	attr_accessor :name, :calories
+  	def initialize(name, calories)
+    		@name = name
+		@calories = calories
+  	end
+  	def healthy?
     # your code here
-  end
-  def healthy?
-    # your code here
-  end
-  def delicious?
-    # your code here
-  end
+		if (@calories < 200)
+			return true
+		else
+			return false
+		end
+  	end
+  	def delicious?
+		return true
+  	end
 end
 
 class JellyBean < Dessert
-  def initialize(flavor)
-    # your code here
-  end
+	attr_accessor :flavor
+	def initialize(flavor)
+		@name = "#{flavor} jelly bean"
+		@calories = 5
+		@flavor = flavor
+	end
+	
+	def delicious?
+		if(@flavor == "licorice")
+			return false
+		else
+			return true
+		end
+	end
 end
